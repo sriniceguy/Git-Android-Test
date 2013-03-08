aad4d1735414ee3274ae508d6ae6b9d2d08debe0
Upendra <upendratarnoju@gallait.com>
2013-03-08 10:10:04 +0530

    upendra first commit

--- Git_AndroidTest/src/com/example/git_androidtest/Git_AndroidTest_1.java
+++ Git_AndroidTest/src/com/example/git_androidtest/Git_AndroidTest_1.java
@@ -1,20 +1,17 @@
 package com.example.git_androidtest;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 
 public class Git_AndroidTest_1 extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-		
-		Log.v("sriram", "frist commit");
-		
+		setContentView(R.layout.main);	
+		Log.v("sriram", "frist commit");	
+		Log.v("Upendra", "first commit");
 		
 	}
 
