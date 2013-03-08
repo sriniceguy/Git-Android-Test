98b5caac7ae8064b2f9aa7d1444c463e4e8772d3
Sriram <sriramayyagari@gallait.com>
2013-03-08 09:57:15 +0530

    frist commit

---
+++ Git_AndroidTest/src/com/example/git_androidtest/Git_AndroidTest_1.java
@@ -0,0 +1,22 @@
+package com.example.git_androidtest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+
+public class Git_AndroidTest_1 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		
+		Log.v("sriram", "frist commit");
+		
+		
+	}
+
+	
+}
